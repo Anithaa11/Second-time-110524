69275780044bc9f7160391569cb04f714a0a412b
Anithaa11 <anithaemail2018@gmail.com>
2024-05-11 14:58:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index.html"))
+    return(render_template("index.html")) 
 
-@app.route("/main",methods=["GET","POST"])
+ @app.route("/main",methods=["GET","POST"])
 def main():
     r = request.form.get("q")
     return(render_template("main.html",r=r))
 
+@app.route("/,prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("prediction.html,r=r")) 
+    
+
 if __name__ == "__main__":
-    app.run()
+   app.run()
+
+    
